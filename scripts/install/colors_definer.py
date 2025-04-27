c23c57f88a04ea7c1f7fa5fa736addcae0839ade
Sophie <miso.png@gmail.com>
2025-04-26 20:02:49 -0500

    feat(colors_definer.py): handle full color definitions (h, s, l, a)

--- scripts/install/colors_definer.py
+++ scripts/install/colors_definer.py
@@ -2,16 +2,18 @@ import json
 
 
 class ColorsDefiner:
-    # TODO: Create a class for each replacer
-    replacers: dict[ str, # ACCENT-COLOR
-        dict[ str, # default, light/dark
-            str | dict[str, int] # random string, s/l/a
-        ]
-    ]
-    # TODO: Create a class for each color
+    replacers: dict[str, dict[str, dict[str, int]]]
+
     colors: dict[str, dict[str, int]]
 
     def __init__(self, filename):
         colors_dict = json.load(open(filename))
         self.replacers = colors_dict["elements"]
         self.colors = colors_dict["colors"]
+
+    def get_color(self, color_name: str):
+        """Get the full color definition (h, s, l, a) for a given color"""
+        if color_name in self.colors:
+            return self.colors[color_name]
+        else:
+            raise ValueError(f"Color '{color_name}' not found in colors definition.")
